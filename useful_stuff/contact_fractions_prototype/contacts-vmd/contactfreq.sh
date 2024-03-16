f68bba853eeb7a4d30be1109d6f659ef2ba2b1ab
recombinatrix <ada.s.quinn+github@gmail.com>
2024-03-16 17:15:23 +1000

    moved to analysis pool

--- useful_stuff/contact_fractions_prototype/contacts-vmd/contactfreq.sh
+++
@@ -1,5 +0,0 @@
-atomselect macro GlyT2 {resname GLY ALA SER THR CYS VAL LEU ILE MET PRO PHE TYR TRP ASP GLU ASN GLN HIS LYS ARG}
-atomselect macro ligand {resname BB4R BB4S C06R C06S E25R E25S G07R G07S H03R H03S}
-
-
-contactFreq {GlyT2} {ligand} 4 0 contact-ligand.out
