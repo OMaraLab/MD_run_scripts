96fa93fa279b1d4408035af5bef383ac35d7233d
Ada <85986321+recombinatrix@users.noreply.github.com>
2025-04-15 14:53:10 +0900

    Update copyq.sh

--- setonix/copyq.sh
+++ setonix/copyq.sh
@@ -5,5 +5,12 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --time=48:00:00
 #SBATCH --account=pawsey0420
+#SBATCH --export=ALL,SSH_AUTH_SOCK
+
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/copyq
+
+# I suspect the --export might be redundant.
+# be careful with this
 
 rsync -vhsrl --chmod=Dg+s -e ssh path/to/source user@data.qriscloud.org.au:/path/to/dest/
